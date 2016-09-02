f11b7f188c31bac57ecc2db63523a80192f0711d
elongeau <emmanuel.longeau@gmail.com>
2016-09-02 22:43:02 +0200

    Move isAFoo at top of spec

--- src/test/scala/RailWaysSpec.scala
+++ src/test/scala/RailWaysSpec.scala
@@ -6,8 +6,9 @@ import org.scalatest.{MustMatchers, WordSpec}
   * @author elongeau
   */
 class RailWaysSpec extends WordSpec with MustMatchers {
+  def isAFoo(s: String): Result[String] = if (s startsWith "Foo") Success(s) else Failure("not a foo")
+
   "bind" should {
-    def isAFoo(s: String): Result[String] = if (s startsWith "Foo") Success(s) else Failure("not a foo")
     def twoTrack = bind(isAFoo)
 
     "transform a function in two track input with a success" in {
