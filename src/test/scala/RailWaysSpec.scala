50b257c0e80272d3aaf9776734bf5d6087ef2cc6
elongeau <emmanuel.longeau@gmail.com>
2016-09-02 22:32:03 +0200

    Extract test function

--- src/test/scala/RailWaysSpec.scala
+++ src/test/scala/RailWaysSpec.scala
@@ -7,12 +7,11 @@ import org.scalatest.{MustMatchers, WordSpec}
   */
 class RailWaysSpec extends WordSpec with MustMatchers {
   "bind (>>)" should {
+    def isAFoo(s: String): Result[String] = if (s startsWith "Foo") Success(s) else Failure("not a foo")
+    def twoTrack = bind(isAFoo)
+
     "transform a function in two track input with a success" in {
-      def isAFoo(s: String): Result[String] = if (s startsWith "Foo") Success(s) else Failure("not a foo")
-      def twoTrack = bind(isAFoo)
-
       twoTrack(Success("FooBar")) mustBe Success("FooBar")
-
     }
   }
 }
