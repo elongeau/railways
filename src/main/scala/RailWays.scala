e65ca113089c84d50bee32c06990fbb096c549dd
elongeau <emmanuel.longeau@gmail.com>
2016-09-02 22:18:10 +0200

    Add railways code
    I did without test

---
+++ src/main/scala/RailWays.scala
@@ -0,0 +1,30 @@
+/**
+  * @author elongeau
+  */
+object RailWays {
+
+  sealed trait Result[A]
+
+  case class Success[A](a: A) extends Result[A]
+
+  case class Failure[A](s: String) extends Result[A]
+
+  object Result {
+    def bind[A, B](f: A => Result[B]): Result[A] => Result[B] = {
+      case Success(a) => f(a)
+      case Failure(s) => Failure(s)
+    }
+
+    def switch[A, B](f: A => B) = (a: A) => Success(f(a))
+
+    implicit class Ops[A, B, C](f: A => Result[B]) {
+      def >>(g: Result[B] => Result[C]) = f andThen g
+
+      def >>=(g: B => Result[C]) = f andThen bind(g)
+
+      def >=>(g: B => C): A => Result[C] = f andThen bind(switch(g))
+    }
+
+  }
+
+}
