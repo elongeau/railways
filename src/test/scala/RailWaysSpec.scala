80fece94c7ab19c682c529079e2ce62511ed7b1d
elongeau <emmanuel.longeau@gmail.com>
2016-09-07 21:37:09 +0200

    Test that /:/ always return the first success

--- src/test/scala/RailWaysSpec.scala
+++ src/test/scala/RailWaysSpec.scala
@@ -93,5 +93,13 @@ class RailWaysSpec extends WordSpec with MustMatchers with TableDrivenPropertyCh
 
       }
     }
+
+    "always return the first success" when {
+      "the 2 functions change the input" in {
+        val parallel = switch(upper _) /:/ switch(lower _)
+          parallel("foo") mustBe Success("FOO")
+
+      }
+    }
   }
 }
