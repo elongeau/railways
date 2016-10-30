9c95e7427fa4b0386e5d5530b1d9b5a86519142e
elongeau <emmanuel.longeau@gmail.com>
2016-10-30 08:54:18 +0100

    Define an implicit to proove that an Option is Railwaysable

--- src/main/scala/Railswaysable.scala
+++ src/main/scala/Railswaysable.scala
@@ -5,3 +5,10 @@ trait Railswaysable[A, B, C, F[_]] {
   def >=>(g: B => F[C]): A => F[C]
 }
 
+object Railswaysable {
+
+  implicit class OptionIsRailswaysable[A, B, C](f: A => Option[B]) extends Railswaysable[A, B, C, Option] {
+    override def >=>(g: (B) => Option[C]): (A) => Option[C] = f(_) flatMap g
+  }
+
+}
