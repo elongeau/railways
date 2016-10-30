44b047e0a5319681bd78e97e4cd4bb3cca8793af
elongeau <emmanuel.longeau@gmail.com>
2016-10-30 08:52:56 +0100

    Test that an Option is Railwaysable

---
+++ src/test/scala/RailswaysableSpec.scala
@@ -0,0 +1,38 @@
+import RailWays.Result
+import RailWays.Result.{Failure, Success}
+import org.scalatest.{MustMatchers, WordSpec}
+import Railswaysable._
+import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor2}
+import org.scalatest.prop.Tables.Table
+
+/**
+  * @author elongeau
+  */
+class RailswaysableSpec extends WordSpec with MustMatchers with TableDrivenPropertyChecks {
+  def isAFoo = (_: String) contains "Foo"
+
+  def isABar = (_: String) contains "Bar"
+
+  "option" should {
+    "be Railswayssable" in {
+      def optFoo(s: String) = if (isAFoo(s)) Some(s) else None
+      def optBar(s: String) = if (isABar(s)) Some(s) else None
+
+      def optFooBar = optFoo _ >=> optBar
+
+      val data: TableFor2[String, Option[String]] = Table(
+        ("Input", "Expected"),
+        ("FooBar", Some("FooBar")),
+        ("FozBar", None),
+        ("FooBaZ", None)
+      )
+
+      forAll(data) { (input: String, expected: Option[String]) =>
+        optFooBar(input) mustBe expected
+
+      }
+
+    }
+  }
+
+}
