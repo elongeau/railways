24c060796110c662eedde7bd0b4f2bda35bb3b2a
elongeau <emmanuel.longeau@gmail.com>
2016-11-01 22:09:56 +0100

    Split syntax and companion object

--- src/main/scala/Railwaysable.scala
+++ src/main/scala/Railwaysable.scala
@@ -5,14 +5,19 @@ trait Railwaysable[F[_]] {
   def chain[A, B, C](f: A => F[B], g: B => F[C]): A => F[C]
 }
 
-object Railwaysable {
+class Syntax {
 
   implicit class RailwaysSyntax[A, B, F[_]](f: A => F[B])(implicit r: Railwaysable[F]) {
     def >=>[C](g: B => F[C]): A => F[C] = r.chain(f, g)
   }
+
+}
+
+object Railwaysable extends Syntax {
 
   implicit object OptionIsRailwaysable extends Railwaysable[Option] {
     override def chain[A, B, C](f: (A) => Option[B], g: (B) => Option[C]): (A) => Option[C] = f(_) flatMap g
   }
 
 }
+
